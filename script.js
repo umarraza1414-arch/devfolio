721850dab6d837468f1ae6ef5ca299e703846634
umarraza1414-arch <umarraza1414@gmail.com>
2026-06-11 17:43:09 +0500

    Enhance slidebar interaction with outside click listener
    Added event listener to close slidebar when clicking outside.

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ const slidebar = document.getElementById("slidebar");
 
 hamburger.addEventListener("click",() =>{
     slidebar.classList.toggle("active");
-})
+});
+
+document.addEventListener("click", (e) => {
+    if (
+        !slidebar.contains(e.target) &&
+        !hamburger.contains(e.target)
+    )  {
+        slidebar.classList.remove("active");
+    }
+});
